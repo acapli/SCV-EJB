ddae55d278d98efadf5208eba0eee7406cf54aee
NoraGonzalez <noritagonzalez@gmail.com>
2014-12-04 08:57:14 -0600

    Update Cajas.java

--- src/java/py/smtr/ejb/entities/Cajas.java
+++ src/java/py/smtr/ejb/entities/Cajas.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  Modificado por nora el 04/12/14
  */
 
 package py.smtr.ejb.entities;
