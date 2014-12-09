88cc73670d2793c487560b58388729f4f21426f8
NoraGonzalez <noritagonzalez@gmail.com>
2014-12-09 06:45:34 -0600

    Update Clientes.java

--- src/java/py/smtr/ejb/entities/Clientes.java
+++ src/java/py/smtr/ejb/entities/Clientes.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+  Modificado por Nora 09/12/14
  */
 
 package py.smtr.ejb.entities;
